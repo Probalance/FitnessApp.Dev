f15dcfed78f295e307c828a76cd8683d86c81b76
Brien <SpydaBeast@users.noreply.github.com>
2018-12-16 17:28:37 -0800

    SG minimum implementation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,14 @@
 
 import { Component, OnInit } from '@angular/core';
-import { SendgridService } from '././service/sendgrid.service'
+import { SendgridService } from '././service/sendgrid.service';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }),
+};
 
 @Component({
   selector: 'app-contact',
@@ -12,24 +20,40 @@ export class ContactComponent implements OnInit {
   title = 'Contact';
   public data: any = []
 
-  constructor(private sendgridService: SendgridService){}
+  //constructor(private sendgridService: SendgridService){}
 
   ngOnInit() { }
+  private url = 'https://us-central1-probalance-214005.cloudfunctions.net/httpEmail?sg_key=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';
 
+  constructor(private _http: HttpClient, sendgridSevice: SendgridService) { }
+  send() {
+    let params: URLSearchParams = new URLSearchParams();
 
-  send(from, text, subject){
-    this.data['to'] = 'work.brien@gmail.com';
-    this.data['from'] = from;
-    this.data['subject'] = subject;
-    this.data['text'] = text;
-    console.log(this.data);
-    this.sendgridService.send(this.data)
-    console.log("Testing http data service: " + from + " " + subject+ " " + text);
+    params.set('to', 'work.brien@gmail.com');
+    params.set('from', 'you@yoursupercoolapp.com');
+    params.set('subject', 'test-email');
+    params.set('content', 'Hey! Your API implementation work!');
+
+    return this._http.post(this.url, params, httpOptions).subscribe(
+      res => {
+        console.log(res);
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client Side Error : " + err.error.message)
+          console.log("Here is my payload" + params)
+        } else {
+          console.log("Server Side Error  : " + err.message + " " + err.type)
+          console.log("Here is my payload " + params)
+        }
+
+      });
+
+    // grecaptcha.ready(function () {
+    //   grecaptcha.execute('6Lcw2WkUAAAAAM4rpKj9eLjvKwcjVOuAaPT3Xkmx', { action: 'homepage' })
+    //     .then(function (token) {
+    //       document.getElementById('g-recaptcha-response').value = token
+    //     });
+    // });
   }
-// grecaptcha.ready(function () {
-//   grecaptcha.execute('6Lcw2WkUAAAAAM4rpKj9eLjvKwcjVOuAaPT3Xkmx', { action: 'homepage' })
-//     .then(function (token) {
-//       document.getElementById('g-recaptcha-response').value = token
-//     });
-// });
 }
--- src/app/contact/service/sendgrid.service.ts
+++ src/app/contact/service/sendgrid.service.ts
@@ -1,66 +1,77 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-
-import { ContactComponent } from '../contact.component'
-
-import { Observable, of } from 'rxjs';
-
-import { catchError, map, tap } from 'rxjs/operators';
 
 
 const httpOptions = {
-
   headers: new HttpHeaders({
-
     'Content-Type': 'application/json',
-
-    'Authorization': 'SENDGRID_API_KEY'
-
-  })
-
+    'Access-Control-Allow-Origin': '*'
+  }),
 };
-
-
 
 @Injectable({
 
   providedIn: 'root'
 
 })
-
 export class SendgridService {
+  private url = 'https://us-central1-probalance-214005.cloudfunctions.net/httpEmail?sg_key=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';
 
+  constructor(private _http: HttpClient ) { }
+  send(_mail) {
+    let params: URLSearchParams = new URLSearchParams();
 
-  private url = 'https://api.sendgrid.com/v3/mail/send';
+    params.set('to', 'user@example.com');
+    params.set('from', 'you@yoursupercoolapp.com');
+    params.set('subject', 'test-email');
+    params.set('content', 'Hello World');
 
+    return this._http.post(this.url, params, httpOptions).subscribe(
+    res => {
+      console.log(res);
+    },
+    (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        console.log("Client Side Error : " + err)
+        console.log("Here is my payload" + _mail )
+      } else {
+        console.log("Server Side Error  : " + err)
+        console.log("Here is my payload" + _mail )
+      }
 
-  constructor(private _http: HttpClient) { }
-
-
-  send(_mail: ContactComponent) {
-
-    return this._http.post<any>(this.url, _mail, httpOptions).subscribe(
-
-      res => {
-
-        console.log(res);
-
-      },
-
-      (err: HttpErrorResponse) => {
-
-        if (err.error instanceof Error) {
-
-          console.log("Client Side Error ")
-          
-        } else {
-
-          console.log("Server Side Error")
-        }
-
-      });
+     });
+                    // .toPromise()
+                    // .then( res => {
+                    //   console.log(res)
+                    // })
+                    // .catch(err => {
+                    //   console.log(err)
+                    // })
 
   }
 
+
+    // let params = new HttpParams()
+    // params.set('to', _mail.data['to']);
+    // params.set('from', _mail.data['from']);
+    // params.set('subject', _mail.data['subject']);
+    // params.set('content', _mail.data['text']);
+
+    // return this._http.post<any>(this.url, _mail, httpOptions).subscribe(
+      // res => {
+      //   console.log(res.json);
+      // },
+      // (err: HttpErrorResponse) => {
+      //   if (err.error instanceof Error) {
+      //     console.log("Client Side Error ")
+      //     console.log("Here is my payload" + _mail )
+      //   } else {
+      //     console.log("Server Side Error  : " + _mail.data)
+      //     console.log("Here is my payload" + _mail )
+      //   }
+
+      //  });
+      
+  //}
+  
 } 
